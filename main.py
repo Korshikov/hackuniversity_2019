39340cfd65a01be8045157393c58a5abd56c20a0
korshikov <pk.delf@gmail.com>
2019-03-23 17:24:59 +0300

    update database convert product equipment class to list

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pymongo
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.hackuniversity_2019
+
+# productCollection = db.product
+#
+# for product in productCollection.find():
+#     productCollection.update_one({'_id':product['_id']},{'$set':{'equipment_class':list(map(lambda it: it.strip(" '"),product['equipment_class'][1:-1].split(",")))}})
