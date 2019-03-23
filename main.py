a9bbc6f233cb37a4850dc1276021ce969cd4fbcf
korshikov <pk.delf@gmail.com>
2019-03-23 21:08:17 +0300

    add datetime deadline into order collection

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from datetime import datetime
+
 import pymongo
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.hackuniversity_2019
 
-# productCollection = db.product
-#
-# for product in productCollection.find():
-#     productCollection.update_one({'_id':product['_id']},{'$set':{'equipment_class':list(map(lambda it: it.strip(" '"),product['equipment_class'][1:-1].split(",")))}})
+productCollection = db.product
+orderCollection = db.order
+
+for product in productCollection.find():
+    # productCollection.update_one({'_id':product['_id']},{'$set':{'equipment_class':list(map(lambda it: it.strip(" '"),product['equipment_class'][1:-1].split(",")))}})
+    bottleneck = 100000000000000000000000
+    for productStageClass in product['equipment_class']:
+        bottleneck = min(db.equipment.find_one({'class': productStageClass}, sort=[("speed_per_hour", 1)])[
+                             'speed_per_hour'], bottleneck)
+    # print(bottleneck)
+for order in orderCollection.find():
+    orderCollection.update_one({'_id': order['_id']},
+                               {'$set': {'deadline_date': datetime.strptime(order['deadline'], "%Y-%m-%d")}})
